868993ca692863fa66344a2ee146b3226dd4d133
mberger75 <marc.berger75020@gmail.com>
2022-08-29 11:24:36 +0200

    fix: import and set default marker

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,6 +1,19 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
+import L from 'leaflet';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 import 'leaflet/dist/leaflet.css';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function Map({ loc }) {
     const position = loc.split(',');
