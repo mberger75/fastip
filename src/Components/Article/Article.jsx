57594497e5fe75b39c8211d287d8283884d7139e
mberger75 <marc.berger75020@gmail.com>
2022-08-29 10:15:04 +0200

    feat: link wiki in footer

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -5,6 +5,7 @@ function Article() {
             <p>An IP address is a string of numbers separated by periods. IP addresses are expressed as a set of four numbers — an example address might be 192.158.1.38. Each number in the set can range from 0 to 255. So, the full IP addressing range goes from 0.0.0.0 to 255.255.255.255.</p>
             <p>IP addresses are not random. They are mathematically produced and allocated by the Internet Assigned Numbers Authority (IANA), a division of the Internet Corporation for Assigned Names and Numbers (ICANN).</p>
             <p>ICANN is a non-profit organization that was established in the United States in 1998 to help maintain the security of the internet and allow it to be usable by all. Each time anyone registers a domain on the internet, they go through a domain name registrar, who pays a small fee to ICANN to register the domain.</p>
+            <a href="https://en.wikipedia.org/wiki/IP_address" target="_blank">Learn more about IP Address</a>
         </article>
     );
 }
