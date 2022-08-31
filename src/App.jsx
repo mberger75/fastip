c305655b0510520db37fa78ec02d9cc0ac83e682
mberger75 <marc.berger75020@gmail.com>
2022-08-31 10:31:34 +0200

    feat: notify user if adblocker is blocking the search

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,32 @@ import Article from './Components/Article';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [info, setInfo] = useState({});
+  const [isBlocked, setIsBlocked] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const resIp = await fetch('https://api.ipify.org?format=json');
-      const dataIp = await resIp.json();
-
-      const resInfo = await fetch(`https://ipinfo.io/${dataIp.ip}?token=5fd3dbf6cf22cc`);
-      const dataInfo = await resInfo.json();
-      setInfo(dataInfo);
-      setIsLoading(!isLoading);
+      try {
+        const resIp = await fetch('https://api.ipify.org?format=json');
+        const dataIp = await resIp.json();
+  
+        const resInfo = await fetch(`https://ipinfo.io/${dataIp.ip}?token=5fd3dbf6cf22cc`);
+        const dataInfo = await resInfo.json();
+        setInfo(dataInfo);
+        setIsLoading(!isLoading);
+      }
+      catch(error) {
+        setIsBlocked(true);
+        console.log(error);
+      }
     })();
   }, [setInfo]);
 
   return (
     <div className="App">
       <h1>Fast IP 🚀</h1>
-      <p className={isLoading ? 'loader visible' : 'loader'}>Searching...</p>
+      <p className={isLoading ? 'loader visible' : 'loader'}>
+        {isBlocked ? 'Please disable your Adblocker.' : 'Searching...'}
+      </p>
       {isLoading && <div className="skeleton"></div>}
       <section className={!isLoading ? 'visible' : ''}>
         {!isLoading &&
